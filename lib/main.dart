ce436f471397de9103242abb4f504e3aca216ad7
kiythub <kateladera07@gmail.com>
2023-02-16 14:40:18 +0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() => runApp(
+    MaterialApp(
+      title: 'Application Technology and Emerging Technology',
+      theme: ThemeData.light(),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage (),
+    )
+);
+
+
